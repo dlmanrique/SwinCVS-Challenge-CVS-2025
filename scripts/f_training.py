5814cd760f6c1372aa9de4af53d40a9e1880aff3
FranekN <fmaxnowak@gmail.com>
2025-06-09 11:00:03 +0100

    Fix bug causing wrong checkpoint dir set-up

--- scripts/f_training.py
+++ scripts/f_training.py
@@ -1,11 +1,16 @@
 import torch
 import os
+from pathlib import Path
 
 def save_weights(model, config, experiment_name, mAP, best_MAP, epoch):
     if mAP >= best_MAP:
         print(f"New best result (Epoch {epoch+1}), saving weights...")
         best_MAP = mAP
-        checkpoint_dir = os.path.join(config.TRAIN.CHECKPOINT_PATH, f'{experiment_name}_bestMAP.pt')
+        if config.TRAIN.CHECKPOINT_PATH == None:
+            checkpoint_path = Path.cwd() / 'weights'
+        else:
+            checkpoint_path = config.TRAIN.CHECKPOINT_PATH
+        checkpoint_dir = os.path.join(checkpoint_path, f'{experiment_name}_bestMAP.pt')
         torch.save(model.state_dict(), checkpoint_dir)
     else:
         print('\n')
